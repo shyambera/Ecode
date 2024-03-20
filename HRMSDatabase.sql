e65e96a75a0de506736d4de2143db9ee6d6af3f9
Shyam <92642242+shyambera@users.noreply.github.com>
2024-03-20 15:10:14 +0530

    comment in database
    some data is commented.

--- HRMSDatabase.sql
+++ HRMSDatabase.sql
@@ -107,132 +107,132 @@ CREATE TABLE IF NOT EXISTS public.job_seeker_cv_educations
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS public.job_seeker_cv_experiences
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    job_seeker_cv_id integer NOT NULL,
-    job_position_id integer NOT NULL,
-    workplace_name character varying(100) NOT NULL,
-    start_date date NOT NULL,
-    quit_date date,
-    created_at timestamp with time zone NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.job_seeker_cv_experiences
+-- (
+--     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     job_seeker_cv_id integer NOT NULL,
+--     job_position_id integer NOT NULL,
+--     workplace_name character varying(100) NOT NULL,
+--     start_date date NOT NULL,
+--     quit_date date,
+--     created_at timestamp with time zone NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.job_seeker_cv_images
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    job_seeker_cv_id integer NOT NULL,
-    url character varying NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.job_seeker_cv_images
+-- (
+--     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     job_seeker_cv_id integer NOT NULL,
+--     url character varying NOT NULL,
+--     created_at timestamp with time zone NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.job_seeker_cv_languages
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    job_seeker_cv_id integer NOT NULL,
-    language_id character(2) NOT NULL,
-    level smallint NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.job_seeker_cv_languages
+-- (
+--     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     job_seeker_cv_id integer NOT NULL,
+--     language_id character(2) NOT NULL,
+--     level smallint NOT NULL,
+--     created_at timestamp with time zone NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.job_seeker_cv_skills
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    job_seeker_cv_id integer NOT NULL,
-    name character varying(100) NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.job_seeker_cv_skills
+-- (
+--     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     job_seeker_cv_id integer NOT NULL,
+--     name character varying(100) NOT NULL,
+--     created_at timestamp with time zone NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.job_seeker_cv_web_sites
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    job_seeker_cv_id integer NOT NULL,
-    web_site_id smallint NOT NULL,
-    address character varying(200) NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.job_seeker_cv_web_sites
+-- (
+--     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     job_seeker_cv_id integer NOT NULL,
+--     web_site_id smallint NOT NULL,
+--     address character varying(200) NOT NULL,
+--     created_at timestamp with time zone NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.job_seeker_cvs
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    job_seeker_id integer NOT NULL,
-    cover_letter character varying(300) NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.job_seeker_cvs
+-- (
+--     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     job_seeker_id integer NOT NULL,
+--     cover_letter character varying(300) NOT NULL,
+--     created_at timestamp with time zone NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.job_seekers
-(
-    user_id integer NOT NULL,
-    first_name character varying(50) NOT NULL,
-    last_name character varying(50) NOT NULL,
-    identity_number character(11) NOT NULL,
-    birth_date date NOT NULL,
-    cv_id integer,
-    PRIMARY KEY (user_id)
-);
+-- CREATE TABLE IF NOT EXISTS public.job_seekers
+-- (
+--     user_id integer NOT NULL,
+--     first_name character varying(50) NOT NULL,
+--     last_name character varying(50) NOT NULL,
+--     identity_number character(11) NOT NULL,
+--     birth_date date NOT NULL,
+--     cv_id integer,
+--     PRIMARY KEY (user_id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.job_seekers_favorite_job_adverts
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    job_seeker_id integer NOT NULL,
-    job_advert_id integer NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS public.job_seekers_favorite_job_adverts
+-- (
+--     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     job_seeker_id integer NOT NULL,
+--     job_advert_id integer NOT NULL
+-- );
 
-CREATE TABLE IF NOT EXISTS public.languages
-(
-    id character(2) NOT NULL,
-    name character varying(50) NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.languages
+-- (
+--     id character(2) NOT NULL,
+--     name character varying(50) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.mernis_activations
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    user_id integer NOT NULL,
-    is_approved boolean NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    approval_date time with time zone,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.mernis_activations
+-- (
+--     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     user_id integer NOT NULL,
+--     is_approved boolean NOT NULL,
+--     created_at timestamp with time zone NOT NULL,
+--     approval_date time with time zone,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.users
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    email character varying(100) NOT NULL,
-    password character varying(100) NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    is_active boolean NOT NULL,
-    is_deleted boolean NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.users
+-- (
+--     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     email character varying(100) NOT NULL,
+--     password character varying(100) NOT NULL,
+--     created_at timestamp with time zone NOT NULL,
+--     is_active boolean NOT NULL,
+--     is_deleted boolean NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.web_sites
-(
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 32767 CACHE 1 ),
-    name character varying(50) NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.web_sites
+-- (
+--     id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 32767 CACHE 1 ),
+--     name character varying(50) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.working_times
-(
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 32767 CACHE 1 ),
-    name character varying(50) NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.working_times
+-- (
+--     id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 32767 CACHE 1 ),
+--     name character varying(50) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS public.working_types
-(
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 32767 CACHE 1 ),
-    name character varying(50) NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS public.working_types
+-- (
+--     id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+--     id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 32767 CACHE 1 ),
+--     name character varying(50) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
 ALTER TABLE public.company_staff_verifications
     ADD FOREIGN KEY (user_id)
